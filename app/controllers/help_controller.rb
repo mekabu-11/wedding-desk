375e0d6e644df0117d3f5e6b1e542e1cbf45bb8e
mekabu-zun2 <axarb0k@gmail.com>
2026-09-07 22:38:02 +0900

    Fix styles for in-app help page

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -3,8 +3,16 @@ class HelpController < ApplicationController
 
   def show
     page = Rails.root.join("docs", "usage.html").read
+    page = page.sub(%r{<style>.*?</style>}m, '<link rel="stylesheet" href="/help/usage.css">')
+    page = page.gsub(' style="margin-top:50px"', '')
     page = page.gsub('src="screenshots/', 'src="/help/screenshots/')
     render html: page.html_safe, layout: false
+  end
+
+  def stylesheet
+    send_file Rails.root.join("docs", "usage.css"),
+      type: "text/css",
+      disposition: "inline"
   end
 
   def screenshot
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   resource :session, only: %i[new create destroy]
   get "help", to: "help#show", as: :help
+  get "help/usage.css", to: "help#stylesheet", as: :help_stylesheet
   get "help/screenshots/:name", to: "help#screenshot", as: :help_screenshot
   resource :wedding, only: %i[new create edit update]
   resources :documents, only: %i[index new create show destroy] do
